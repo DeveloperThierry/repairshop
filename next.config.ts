d30328c8d6e8fee105e127aa44545bd21ce1c6d3
developerthierry <developerthierry@gmail.com>
2026-08-19 19:18:37 +0000

    Create Homepage

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "3000-firebase-capgit-1781909507590.cluster-lrhkgnsygfb7ovnbvdt2fflxme.cloudworkstations.dev",
+  ],
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google'
 import "./globals.css";
-
+ 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${roboto.className}  h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
